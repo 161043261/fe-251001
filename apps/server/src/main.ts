70a8d9d3e1b14f3980ab1493ad14a2ec27afd5d0
Tiancheng <yukino161043261@gmail.com>
2025-10-15 21:19:48 +0800

    feat: Introduce new features

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  console.log(__dirname);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
