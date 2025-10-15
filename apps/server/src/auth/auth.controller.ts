d73340ddafa682b52c9f4ae1197dfb85d5909966
Tiancheng <yukino161043261@gmail.com>
2025-10-15 15:32:06 +0800

    feat: Introduce new features

--- apps/server/src/auth/auth.controller.ts
+++ apps/server/src/auth/auth.controller.ts
@@ -1,17 +1,21 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginAuthDto } from './dto/login-auth.dto';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // curl -X POST \
-  // -H "Content-Type:application/json" \
-  // -d '{"username":"whoami","password":"123456"}' \
-  // http://localhost:3000/auth/login
+  // curl http://localhost:3000/auth/login -X POST -H "Content-Type: application/json" -d '{"username": "whoami", "password": "123456"}'
   @Post('login')
   create(@Body() loginAuthDto: LoginAuthDto) {
     return this.authService.login(loginAuthDto);
+  }
+
+  // curl http://localhost:3000/auth/register -X POST -H "Content-Type: application/json" -d '{"username": "user", "password": "123456"}'
+  @Post('register')
+  async register(@Body() createAuthDto: CreateAuthDto) {
+    return this.authService.register(createAuthDto);
   }
 }
--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -1,19 +1,24 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { LoginAuthDto } from './dto/login-auth.dto';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
-  // curl http://localhost:3000/auth/login -X POST -H "Content-Type: application/json" -d '{"username": "u", "password": "pppp"}'
+  // curl http://localhost:3000/auth/login -X POST -H "Content-Type: application/json" -d '{"username": "whoami", "password": "123456"}'
   login(loginAuthDto: LoginAuthDto) {
     const { username, password } = loginAuthDto;
 
     const userGetById = {
       id: 1,
-      username: 'u',
-      password: 'pppp',
+      username: 'whoami',
+      password: '123456',
     };
     if (username !== userGetById.username) {
       throw new HttpException('username error', HttpStatus.BAD_REQUEST);
@@ -27,6 +32,27 @@ export class AuthService {
     };
   }
 
-  // register() {
-  // }
+  async register(createAuthDto: CreateAuthDto) {
+    const { username, password } = createAuthDto;
+
+    const res = await this.prismaService.user.findFirst({
+      where: {
+        username,
+      },
+    });
+
+    if (res) {
+      throw new HttpException(
+        'username already exists',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return this.prismaService.user.create({
+      data: {
+        username,
+        password,
+      },
+    });
+  }
 }
